7bac15d4a0a1f2c681f61d68de0990f06402a71a
Kanwar07 <anmolkanwar07@gmail.com>
2024-04-02 02:46:45 +0530

    projectscarousel

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,25 +8,34 @@ function Projects() {
 
   return (
     <div className={projectstyle.main}>
-      <div className="grid grid-cols-4 gap-2 w-[100%] pt-4 pb-4 z-10 max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-2 max-xl:grid-cols-3">
-        {projects.length > 0 &&
-          projects.map((project) => {
-            const { title, _id, image } = project;
-            return (
-              <div key={_id} className="flex flex-col items-center p-4">
-                <img src={image.url} alt={title} className="mb-6" />
-                <div>{title}</div>
-              </div>
-            );
-          })}
-      </div>
+      <motion.div className={projectstyle.carousel}>
+        <motion.div drag="x" className={projectstyle.innercarousel}>
+          {projects.length > 0 &&
+            projects.map((project) => {
+              const { title, _id, image } = project;
+              return (
+                <motion.div
+                  key={_id}
+                  className={projectstyle.item}
+                  initial={{ x: 0 }}
+                  animate={{ x: "-1525%" }}
+                  transition={{
+                    duration: 55,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                >
+                  <img src={image.url} alt={title} />
+                  <div className="pt-6 text-center">{title}</div>
+                </motion.div>
+              );
+            })}
+        </motion.div>
+      </motion.div>
       <motion.div
-        initial={{ x: 0 }}
-        animate={{ x: "-110%" }}
-        transition={{ duration: 20, repeat: Infinity, repeatType: "loop" }}
         className={projectstyle.text}
         style={{
-          fontSize: "24vw",
+          fontSize: "20vw",
           color: "rgb(0, 0, 0, 0.2)",
         }}
       >
